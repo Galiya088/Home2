a9c74b96b103906e4138f728207c775674c9f8f9
Galiya <safaga71@mail.ru>
2023-01-21 12:23:19 +0300

    Program

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -8,5 +8,4 @@
 Console.Write("Введите число трехзначное число: ");
 int num1 = int.Parse(Console.ReadLine()); 
 int num2 = (num1 / 10) % 10;
-// int num3 = num2 % 10;
 Console.Write($"Вторая цифра числа -> " + num2);
